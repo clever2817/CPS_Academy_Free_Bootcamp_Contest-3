d997351afa83638cf0cd237525fee8fff3d4d265
Menhajul Abir <cleverprogrammer404@gmail.com>
2025-07-24 21:02:26 +0600

    Problem D Solved

---
+++ D-Not_Too_Hard.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, x;
+
+    cin >> n >> x;
+
+    int a[n];
+
+    int sum = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+
+        if(a[i] <= x)
+        {
+            sum += a[i];
+        }
+    }
+
+    cout << sum << "\n";
+
+    return 0;
+}
